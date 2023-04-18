075df8325a1dd9c917550aa27e1335efb68227f0
accrane <austincrane@gmail.com>
2023-04-18 10:28:21 -0400

    block kara

--- header.php
+++ header.php
@@ -21,6 +21,14 @@
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <meta name="viewport" content="width=device-width" />
+
+
+<?php if( is_page( 2647 ) ) { ?>
+	<meta name="robots" content="noindex">
+	<!-- We're blocking only her -->
+<?php } ?>
+
+
 <title><?php wp_title( '|', true, 'right' ); ?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
